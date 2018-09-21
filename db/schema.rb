0872e9c67a6dc00785570536a9a53dee2dbf1f31
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-21 02:04:12 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,11 @@
 
 ActiveRecord::Schema.define(version: 20180913010554) do
 
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
